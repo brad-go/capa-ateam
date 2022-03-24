a7b2b3e879f172e004c0292f618d5a20877b1256
고동현 <dhjk35@naver.com>
2022-03-24 21:10:39 +0900

    :recycle: Refactor: 공통 컴포넌트 export 설정

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { default as BlankBoard } from './BlankBoard/BlankBoard';
+export { default as Button } from './Button/Button';
+export { default as CheckBox } from './CheckBox/CheckBox';
+export { default as Toggle } from './Toggle/Toggle';
